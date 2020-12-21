d3bd1937adfb24fb66ddcc48705cca315ee6ab64
Nathan Marley <nathan.marley@gmail.com>
2020-12-21 20:43:08 -0300

    Add test

--- src/first.rs
+++ src/first.rs
@@ -31,7 +31,40 @@ impl List {
             Link::More(node) => {
                 self.head = node.next;
                 Some(node.elem)
-            },
+            }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
