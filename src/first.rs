de16faaba02b9cc751abe53498f26c84d0a6f7ad
Nathan Marley <nathan.marley@gmail.com>
2020-12-21 20:37:45 -0300

    Impl List some more

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -8,4 +10,32 @@ struct Node {
 enum Link {
     Empty,
     More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+            },
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            },
+        };
+        result
+    }
 }
