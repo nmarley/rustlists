d68514236b9d3451db45d1c1c8d16faea0424115
Nathan Marley <nathan.marley@gmail.com>
2020-12-22 18:30:19 -0300

    Add append and tail

--- src/third.rs
+++ src/third.rs
@@ -15,4 +15,19 @@ impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
+
+    pub fn append(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
 }
