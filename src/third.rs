3375cd801414fcfcc905b543d569e372c854133b
Nathan Marley <nathan.marley@gmail.com>
2020-12-22 18:34:25 -0300

    Add tests

--- src/third.rs
+++ src/third.rs
@@ -35,3 +35,30 @@ impl<T> List<T> {
         self.head.as_ref().map(|node| &node.elem)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.append(1).append(2).append(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure that empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+}
