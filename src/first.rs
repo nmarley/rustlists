825ef7aadca7abc82529a1e24e9ef28826333e11
Nathan Marley <nathan.marley@gmail.com>
2021-01-15 12:13:28 -0300

    Use Option::take in lieu of mem::replace

--- src/first.rs
+++ src/first.rs
@@ -1,5 +1,3 @@
-use std::mem;
-
 pub struct List<T> {
     head: Option<Box<Node<T>>>,
 }
@@ -17,14 +15,14 @@ impl<T> List<T> {
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, None),
+            next: self.head.take(),
         });
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<T> {
         let result;
-        match mem::replace(&mut self.head, None) {
+        match self.head.take() {
             None => result = None,
             Some(node) => {
                 result = Some(node.elem);
