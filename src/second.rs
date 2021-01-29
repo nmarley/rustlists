ade4159512c449f65b3476c5340e4a6a130e3190
Nathan Marley <nathan.marley@gmail.com>
2021-01-28 23:28:46 -0300

    Fix iter lifetimes and deref

--- src/second.rs
+++ src/second.rs
@@ -39,22 +39,22 @@ impl<T> List<T> {
         IntoIter(self)
     }
 
-    pub fn iter(&self) -> Iter<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
         Iter {
-            next: self.head.map(|node| &node),
+            next: self.head.as_deref(),
         }
     }
 }
 
-pub struct Iter<T> {
-    next: Option<&Node<T>>,
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
 }
 
-impl<T> Iterator for Iter<T> {
-    type Item = &T;
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         self.next.map(|node| {
-            self.next = node.next.map(|node| &node);
+            self.next = node.next.as_deref();
             &node.elem
         })
     }
@@ -149,10 +149,10 @@ mod test {
         list.push(2);
         list.push(3);
 
-        let iter = list.iter();
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(1));
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), None);
     }
 }
